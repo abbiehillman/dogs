c7aac172e5cecc3ea40f8c6b34a6b8f8640febbe
audreye1 <audreyestanislao@gmail.com>
2025-03-17 08:47:22 -0700

    added retrofit/service/parcelize

--- app/src/main/java/com/example/dogs/Item.kt
+++ app/src/main/java/com/example/dogs/Item.kt
@@ -1,7 +1,10 @@
 package com.example.dogs
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class Item(
     var message:  String,
     var status: Boolean
-)
+): Parcelable
