55ced33b3c124e33af6c57f1d339a37fd7f1042c
abbiehillman <abigailhillman18@gmail.com>
2025-03-19 10:01:02 -0700

    first commit with background and title

--- app/src/main/java/com/example/dogs/MainActivity.kt
+++ app/src/main/java/com/example/dogs/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.dogs
 
+import android.app.Activity
+import android.app.ProgressDialog.show
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.example.dogs.databinding.ActivityMainBinding
 
@@ -21,17 +25,19 @@ class MainActivity : AppCompatActivity() {
         breedSearch = findViewById(R.id.button_main_searchBreed)
         randomPuppy = findViewById(R.id.button_main_random)
 
-        breedSearch.setOnClickListener {
+
+
+        randomPuppy.setOnClickListener {
             Toast.makeText(this, "Clicked 1", Toast.LENGTH_SHORT).show()
             val newRandomActivityIntent = Intent(
                 this,
                 RandomActivity::class.java
             )
-            startActivity(newRandomActivityIntent)
+            newRandomActivityIntent.startActivity()
 
         }
 
-        randomPuppy.setOnClickListener {
+        breedSearch.setOnClickListener {
             Toast.makeText(this, "Clicked 2", Toast.LENGTH_SHORT).show()
 
         }
